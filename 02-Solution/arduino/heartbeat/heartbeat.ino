eb11d6c8c656383d084805f7153315c27f894ac1
Roland Bruggmann <roland.bruggmann@gmx.net>
2017-09-27 22:29:50 +0200

    init

---
+++ 02-Solution/arduino/heartbeat/heartbeat.ino
@@ -0,0 +1,5 @@
+// This is H34RTB34T.
+//
+// Biomedical Instrumentation BI and Introduction to Electrical Engineering EE,
+// Master's program in Biomedical Engineering BME, University of Bern.
+// Roland Bruggmann, 2017.
---
+++ 02-Solution/matlab/heartbeat.m
@@ -0,0 +1,21 @@
+% This is H34RTB34T.
+%
+%**************************************************************************
+% Master's program in Biomedical Engineering BME, University of Bern.
+% Course:       Biomedical Instrumentation BI 
+%               and 
+%               Introduction to Electrical Engineering EE
+% Lab:           
+% Autor:        Roland Bruggmann
+% Date:         27. Sep  2017
+% Version:      1.0
+%--------------------------------------------------------------------------
+% Version History:
+% 1.0: Create, 27.09.2017
+%----------
+% 
+%**************************************************************************
+
+clear all;  %Clear workspace
+clc;        %Clear command window
+close all;  %Close all figures
